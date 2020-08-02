4cfdeb4ee041bce3f38749a63d716be963252639
gangchankim <superremnant@naver.com>
2020-08-03 01:35:46 +0900

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-docker-compose -f ./Server/creditcoin-with-gateway.yaml down
-docker-compose -f ./Server/creditcoin-default.yaml down
-docker-compose -f ./Server/creditcoin-with-gateway.yaml pull
-docker-compose -f ./Server/creditcoin-default.yaml pull
-docker-compose -f ./Server/creditcoin-with-gateway.yaml up
+docker-compose -f ~/Server/creditcoin-with-gateway.yaml down
+docker-compose -f ~/Server/creditcoin-default.yaml down
+docker-compose -f ~/Server/creditcoin-with-gateway.yaml pull
+docker-compose -f ~/Server/creditcoin-default.yaml pull
+docker-compose -f ~/Server/creditcoin-with-gateway.yaml up
