41ac4b981e25d9816473e55940db2c4ddaf4eafa
gangchankim <superremnant@naver.com>
2020-08-03 01:40:43 +0900

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-docker-compose -f ./Client/creditcoin-client.yaml down
-docker-compose -f ./Client/creditcoin-client.yaml pull
-docker-compose -f ./Client/creditcoin-client.yaml up -d
+docker-compose -f ./CreditcoinDocs-Mainnet/Client/creditcoin-client.yaml down
+docker-compose -f ./CreditcoinDocs-Mainnet/Client/creditcoin-client.yaml pull
+docker-compose -f ./CreditcoinDocs-Mainnet/Client/creditcoin-client.yaml up -d
